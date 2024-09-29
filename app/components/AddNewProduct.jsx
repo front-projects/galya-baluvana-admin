84949948bb4ca79b3252e436349c2634247c306a
Front-projects <sasnykvlad2@gmail.com>
2024-09-29 13:50:15 +0300

    upd

--- app/components/AddNewProduct.jsx
+++ app/components/AddNewProduct.jsx
@@ -48,6 +48,7 @@ export default function AddNewProduct({ category }) {
         coockingUa: '',
         images: '',
       });
+      setResource([]);
       setIsEditing(false);
       router.refresh();
     } else {
